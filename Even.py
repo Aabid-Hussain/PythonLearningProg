587806fe1d6827c9cfcf4f569697317b56d69de0
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-01-27 05:56:09 -0800

    This is my first commit

---
+++ Even.py
@@ -0,0 +1,7 @@
+# 2	Write a program to print all even numbers between 1 to 50
+
+print "Numbers between 1 to 50:"
+
+for i in range(1,50):
+    if i%2==0:
+        print i
---
+++ Odd.py
@@ -0,0 +1,7 @@
+# 3	Write a program to print all off numbers between 1 to 50
+
+print "Odd Number between 1 to 50"
+
+for i in range(1,50):
+    if i%2 != 0:
+        print i
