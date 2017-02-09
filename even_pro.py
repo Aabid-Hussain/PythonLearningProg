ddf8848cc4616effacca9b2c5d9040cd0b2cf672
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-02-09 04:35:38 -0800

    This is practise commit

--- even_pro.py
+++ even_pro.py
@@ -2,7 +2,7 @@
 
 num = input("Enter your number to find Even/Odd:")
 
-if (num%2==0):
+if not num%2: #if num%2: will return odd values and if not num%2: will return even value
     print "The number is Even"
 else:
     print "The number is Odd"
