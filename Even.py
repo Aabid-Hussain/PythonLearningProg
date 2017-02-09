f4fe2caa5ad07ca0b92793f5c97bc1ca472f17a2
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-02-09 04:34:14 -0800

    This is practise commit

--- Even.py
+++ Even.py
@@ -3,5 +3,6 @@
 print "Numbers between 1 to 50:"
 
 for i in range(1,50):
-    if i%2==0:
+    if i%2==0: #can be used "if not i%2:" --> return 0
+        # if i%2: --> return any value but not 0
         print i
