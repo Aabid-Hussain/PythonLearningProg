9343115d131b62c7afb8e1190ed566df95d3ff5a
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-05-07 08:31:14 -0700

    Started coding MagicGame

---
+++ Challenge/MagicGame.py
@@ -0,0 +1,12 @@
+import random
+
+#warrior & Battle Class
+#Warriors will have names, health, and attack and block maximum
+#they will have the capabilities to attack and block random amounts
+#Attack random() 0.0 to 1.0 * maxAttack +.5
+#Block will use random()
+# Battle Class capability of looping until 1 warrior dies
+#Warriors will each get a turn to attack each other
+#Function gets 2 warriors
+#1 warrior attacks the other
+# Attacks and Blocks be integers
