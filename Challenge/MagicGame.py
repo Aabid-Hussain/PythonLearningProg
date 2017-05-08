621504c806787ad820892f908e062f2c5d804cae
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-05-08 05:13:02 -0700

    Coded MagicGame

--- Challenge/MagicGame.py
+++ Challenge/MagicGame.py
@@ -1,12 +1,78 @@
 import random
+import math
 
 #warrior & Battle Class
-#Warriors will have names, health, and attack and block maximum
-#they will have the capabilities to attack and block random amounts
-#Attack random() 0.0 to 1.0 * maxAttack +.5
-#Block will use random()
-# Battle Class capability of looping until 1 warrior dies
-#Warriors will each get a turn to attack each other
-#Function gets 2 warriors
-#1 warrior attacks the other
-# Attacks and Blocks be integers
+class Warrior(object):
+
+    def __init__(self,name="warrior",health=0,attackMax=0,blockMax=0):
+        self.name = name
+        self.health = health
+        self.attackMax = attackMax
+        self.blockMax = blockMax
+
+    def attack(self):
+        atkAmt = self.attackMax * (random.random() + 0.5)
+
+        return atkAmt
+
+    def block(self):
+        blockAmt = self.blockMax * (random.random() + 0.5)
+
+        return blockAmt
+
+class Battle(object):
+
+    def startFight(self,warrior1,warrior2):
+
+        while True:
+
+            if self.getAttkResult(warrior1,warrior2) == "Game Over":
+
+                print "Game Over"
+                break
+
+            if self.getAttkResult(warrior2,warrior1) == "Game Over":
+
+                print "Game Over"
+                break
+
+    @staticmethod
+    def getAttkResult(warriorA, warriorB):
+
+        warriorAAttkAmt = warriorA.attack()
+
+        warriorBBlkAmt = warriorB.block()
+
+        damage2warriorB = math.ceil( warriorAAttkAmt - warriorBBlkAmt)
+
+        warriorBHealth = warriorB.health - damage2warriorB
+
+        print("{} attacks {} and deals {} damage".format(warriorA.name,
+                        warriorB.name, damage2warriorB))
+
+        print("{} is down to {} health".format(warriorB.name,
+                                               warriorB.health))
+
+        if warriorB.health <= 0:
+            print("{} has Died and {} is Victorious".format(warriorB.name,
+                                                            warriorA.name))
+
+            return "Game Over"
+        else:
+            return "Fight Again"
+
+def main():
+
+        # Create 2 Warriors
+        paul = Warrior("Paul", 50, 20, 10)
+        sam = Warrior("Sam", 50, 20, 10)
+
+        # Create Battle object
+        battle = Battle()
+
+        # Initiate Battle
+        battle.startFight(paul, sam)
+
+main()
+
+
