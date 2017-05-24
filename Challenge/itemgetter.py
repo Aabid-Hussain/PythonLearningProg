ddb186e02970da2a9ec0596b3577eedfa2e3a6a0
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-05-24 04:26:06 -0700

    worked on itemgetter logic

--- Challenge/itemgetter.py
+++ Challenge/itemgetter.py
@@ -4,11 +4,11 @@ def itemgetter(*items):
     if len(items) == 1:
         item = items[0]
         def g(ob):
-            print(ob)
+            # print(ob)
             return ob[item]
     else:
         def g(ob):
-            print(ob)
+            # print(ob)
             return tuple(ob[item] for item in items)
     return g
 
