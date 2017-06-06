2b8df67e4632a87b7f34f882999f5042bb2dbc52
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-06-05 23:59:31 -0700

    worked on logger code and now it is working fine

--- Challenge/logger.py
+++ Challenge/logger.py
@@ -1,9 +1,55 @@
 '''
 1. define function logger_func(func)
+
 2. to create log file we need to import logging package.
 2. create log file name and log data as debugging =INFO
 3. use wrapper function to wrap the data
 4. return wrapper function
+
+'''
+
+import logging
+
+def logger_func(log_func):
+
+    logging.basicConfig(filename='{}.log'.format(logger_func.__name__),
+                            level= logging.INFO)
+
+    def wrapper(*args, **kwargs):
+
+        logging.info("Ran with arguments {} and keywords {}".format(args, kwargs))
+
+        return log_func(*args, **kwargs)
+
+    return wrapper
+
+
+@logger_func
+def fact(n):
+
+    if n == 1:
+        return 1
+    else:
+        return n * fact(n-1)
+
+for i in range(1,10):
+    fact(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 '''
 
@@ -29,3 +75,5 @@ def addition(name, age):
 
 
 addition('Aabid', 25)
+
+'''
