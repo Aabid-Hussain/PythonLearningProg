3e04ab54079d9f40284c07b1dbe26e9e6bb8e49e
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-06-11 10:12:35 -0700

    worked on project character.py

---
+++ OOP/character.py
@@ -0,0 +1,37 @@
+class Direction:
+
+    SOUTH = 's'
+    WEST = 'w'
+    EAST = 'e'
+    NORTH = 'n'
+
+
+class MainSymbol:
+
+    def __init__(self, symbol,startX = 0,
+                 startY = 0, velx =1, vely = 1, trace = "*"):
+
+        self.symbol = symbol
+
+        self.x = startX
+        self.y = startY
+
+        self.velx = velx
+        self.vely = vely
+
+        self.trace = trace
+
+
+    def move(self,direction):
+
+        #Y
+        if direction == Direction.SOUTH:
+            self.y += self.vely
+        elif direction == Direction.NORTH:
+            self.y -= self.vely
+
+        # X
+        elif direction == Direction.EAST:
+            self.x +=self.velx
+        elif direction == Direction.WEST:
+            self.x -= self.velx
