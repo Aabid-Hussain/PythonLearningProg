cf196986c079885e80a28cd7fc14334f71ee6fb5
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-07-06 00:54:27 -0700

    worked on organisation of class

---
+++ Basics/class_org.py
@@ -0,0 +1,63 @@
+class Shape:
+
+    def __init__(self, width=1, height=1):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if value>=0:
+            self.__width = value
+
+        else:
+            print("Please enter positive integer")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value >= 0:
+            self.__height = value
+
+        else:
+            print("Please enter positive integer")
+
+    def getArea(self):
+        return self.__width * self.__height
+
+class Square(Shape):
+    def __init__(self, side):
+        super().__init__(width = 1, height = 1)
+        self.side = side
+
+    def getArea(self):
+        return self.side * self.side
+
+class Rectangle(Shape):
+    def __init__(self, length = 1 , base = 1):
+        super().__init__(length, base)
+        self.length = length
+        self.base = base
+
+obj = Square(12)
+objRect = Rectangle(10,20)
+objRect1 = Rectangle()
+
+print(obj.getArea())
+print(objRect.getArea())
+print(objRect1.getArea())
+
+
+
+
+
+
+
+
+
