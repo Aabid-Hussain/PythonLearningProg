c6d1601a25aed2697809ee531cf3ffeff50c5bf8
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-07-17 05:02:56 -0700

    coded problem related to dict and map

---
+++ HackerRank/Dictionaries and Maps.py
@@ -0,0 +1,22 @@
+
+n =  int(input())
+dict_store_key = list(input().strip().split("\n") for _ in range(n))
+dict_store = {}
+
+# for i in range(len(dict_store_key)-1):
+#     a.append(str(dict_store_key[i]).lstrip("['").rstrip("']").split(" "))
+
+# print(a.items())
+# print(dict_store_key)
+
+for i in dict_store_key:
+    a = str(i).lstrip("['").rstrip("']").split(" ")
+    dict_store[a[0]] = a[1]
+
+for _ in range(n):
+
+    dict_store_value = input().strip()
+    if dict_store_value in dict_store.keys():
+        print("{}={}".format(dict_store_value, dict_store[dict_store_value]))
+    else:
+        print("Not found")
