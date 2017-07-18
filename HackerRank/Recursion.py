42a3c996254772207534f01a35fc29fe5d0c06d5
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-07-18 02:02:21 -0700

    coded problem related to recursion

---
+++ HackerRank/Recursion.py
@@ -0,0 +1,13 @@
+import sys
+
+def factorial(n):
+    # Complete this function
+    if n==0 | n==1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    result = factorial(n)
+    print(result)
