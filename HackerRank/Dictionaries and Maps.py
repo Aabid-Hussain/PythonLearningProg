aa03e2d961dd8cc4283d0dc8fc28f03537151520
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-07-18 02:01:39 -0700

    coded problem related to Dictionaries and maps

--- HackerRank/Dictionaries and Maps.py
+++ HackerRank/Dictionaries and Maps.py
@@ -1,4 +1,4 @@
-
+'''
 n =  int(input())
 dict_store_key = list(input().strip().split("\n") for _ in range(n))
 dict_store = {}
@@ -20,3 +20,18 @@ for _ in range(n):
         print("{}={}".format(dict_store_value, dict_store[dict_store_value]))
     else:
         print("Not found")
+'''
+N = int(input())
+phonebook = {}
+
+for i in range(N):
+    key, value = input().split()
+    phonebook[key] = int(value)
+
+for i in range(N):
+    query = input()
+    #if phonebook.get(query, "Not found") == "Not found":
+    if not phonebook.get(query):
+        print("Not found")
+    else:
+        print(query, '=', phonebook.get(query), sep='')
