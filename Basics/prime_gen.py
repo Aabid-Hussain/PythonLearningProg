26aaeb9aec740955c30897aeb70aa2138d3655bc
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-07-19 03:24:20 -0700

    Working on generator in python

--- Basics/prime_gen.py
+++ Basics/prime_gen.py
@@ -1,50 +1,25 @@
 def is_prime(n):
-    if n == 1:
+    if n == 1 or n == 0:
         return False
     elif n == 2 or n == 3:
         return True
     else:
-        for i in range(2,int(n**0.5)+1):
-            if not n%i :
+        for i in range(2, int(n**0.5)+1):
+            if not n%i:
                 return False
         else:
             return True
 
-# for n in range(2,101):
-#     if is_prime(n):
-#         print("{}".format(n), sep="\n")
-
-def prime_generator():
+def prime_gen():
     n = 2
     while True:
         if is_prime(n):
             yield n
+
         n += 1
 
-for i in prime_generator():
+for i in prime_gen():
     if i>100:
         break
+
     print(i)
-
-'''   
-# primes up to 'max'
-def primes_max(max):
-    yield 2
-    for n in range(3, max, 2):
-        if is_prime(n):
-            yield n
-
-# the first 'count' primes
-def primes_count(count):
-    counter = 0
-    num = 3
-
-    yield 2
-
-    while counter < count:
-        if is_prime(num):
-            yield num
-            counter += 1
-        num += 2
-
-'''
