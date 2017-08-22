9943157ddad88931e6acc087f64cc905e38f1565
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-08-22 07:45:20 -0700

    commit 7

--- Basics/practise.py
+++ Basics/practise.py
@@ -1,9 +1,17 @@
 import pickle
 
 
-cities = ["Paris", "Dijon", "Lyon", "Strasbourg"]
-fh = open("data.pkl", "rb")
-for inline in fh:
-    print inline
+lines = open("E:\ME\cities_and_times.txt").readlines()
 
-fh.close()
+lines.sort()
+
+cities = []
+
+for line in lines:
+    *c, day, time = line.split()
+    hours, minutes = time.split(":")
+    cities.append((" ".join(c), day,
+                   (int(hours), int(minutes))))
+
+fh = open("E:\ME\cities_and_times.pkl",'wb')
+pickle.dump(cities, fh)
