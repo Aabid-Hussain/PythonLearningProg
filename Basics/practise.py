0b2d8612e0f58b1e580bd8d3a21ed4e964c2406c
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-08-22 06:06:18 -0700

    commit 2

--- Basics/practise.py
+++ Basics/practise.py
@@ -1,14 +1,30 @@
-def g():
-    print("Hi, it's me 'g'")
-    print("Thanks for calling me")
+'''
+def our_decorator(func):
+    def function_wrapper(x):
+        print("Before calling " + func.__name__)
+        func(x)
+        print("After calling " + func.__name__)
+    return function_wrapper
+
+@our_decorator
+def foo(x):
+    print("Hi, foo has been called with " + str(x))
+
+foo("Hi")
+'''
+
+def decor(func):
+    def decor_wrapper(x):
+        print("before function called " + func.__name__)
+        func(x)
+        print("After calling " + func.__name__)
+    return decor_wrapper
+
+@decor
+def foo(x):
+    print("foo is called with " + str(x))
+
+foo("python")
 
 
-def f(func):
-    print("Hi, it's me 'f'")
-    print("I will call 'func' now")
-    func()
-    print("func's real name is " + func.__name__)
 
-f(g)
-
-# to print func name or class name, func.__name__
