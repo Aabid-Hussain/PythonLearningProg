62777e638232bcaae7f258236b26eabca23cee76
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-10-06 08:53:30 -0700

    Working with hacker rank problems

--- HackerRank/Rangoli.py
+++ HackerRank/Rangoli.py
@@ -1,19 +1,18 @@
 import string
 
-alpha = string.ascii_lowercase
+string_value_ascii = string.ascii_lowercase  # your code goes here
+empty_list = []
+size = int(input())
+for ran_value in range(size):
+    temp_str = "-".join(string_value_ascii[ran_value:size])
+    empty_list.append(temp_str[::-1] + temp_str[1:])
 
-n = int(input())
-rangoli = []
-for i in range(n):
-    s = '-'.join(alpha[i:n])
-    rangoli.append(s[::-1]+s[1:])
+width_len = len(empty_list[0])
 
-width = len(rangoli[0])
+# print upper half of the rangoli
+for ran_value in range(size - 1, 0, -1):
+    print(empty_list[ran_value].center(width_len, "-"))
 
-for i in range(n-1,0,-1):
-    print(rangoli[i].center(width,"-"))
-
-for i in range(n):
-    print(rangoli[i].center(width, "-"))
-
-
+# print next half of the rangoli
+for ran_value in range(size):
+    print(empty_list[ran_value].center(width_len, "-"))
