2ed00d28543e6bb9b3f99b3808cd7c79311e56a7
Aabid-Hussain <aabid.hussain@info.com>
2018-01-24 11:36:04 +0530

    python-network-prog-2

---
+++ 57Challenge/chal_4.py
@@ -0,0 +1,50 @@
+import paramiko
+from time import sleep
+from getpass import getpass
+
+
+devices = {
+            'SVR-1': {'ip': '192.168.182.101'}
+}
+
+max_buffer = 65535
+
+commands = ['ls -ltr', 'ps -aux | grep network', 'compgen -u']
+
+username = raw_input("Username: ")
+password = getpass('Password: ')
+
+def clear_buffer(connection):
+    if connection.recv_ready():
+        return connection.recv(max_buffer)
+
+
+for dev in devices.keys():
+    output_file = 'dev' + '_output.txt'
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(devices[dev]['ip'], username=username, password=password,
+                look_for_keys=False, allow_agent=False)
+
+    new_connection = ssh.invoke_shell()
+    output = clear_buffer(new_connection)
+    sleep(2)
+
+    new_connection.send("ps -aux \n")
+
+    output = clear_buffer(new_connection)
+
+
+    with open(output_file, 'wb') as fob:
+        for command in commands:
+            new_connection.send(command + "\n")
+            sleep(2)
+            output = clear_buffer(new_connection)
+            print(output)
+            fob.write(output)
+
+    new_connection.close()
+
+
+
